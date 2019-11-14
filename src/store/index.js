6f75bfbe7df0960940b41161e71711ae97c59bf4
Edison <edirates@gmail.com>
2019-11-14 20:11:36 +0700

    Refactoring for vuex

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,13 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import db from '../configs/firebase'
+import router from '../router/index'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    rooms: [],
     roomId: '',
     master: {
       name: null,
@@ -24,9 +27,68 @@ export default new Vuex.Store({
   mutations: {
     CHANGE_ROOM(state, payload){
       state.roomId = payload
+    },
+    CHANGE_ROOMS(state, payload){
+      state.rooms = payload
     }
   },
   actions: {
+    getFreeRoom (context) {
+      db.collection('rooms').where('available', '==', true)
+      .onSnapshot((querySnapshot) => {
+          let lists = [];
+          querySnapshot.forEach(function(doc) {
+              let room = {
+                id: doc.id,
+                ...doc.data()
+              }
+              lists.push(room);
+          });
+          context.commit('CHANGE_ROOMS', lists)
+      });
+    },
+    createRoom (context) {
+      db.collection("rooms").add({
+        host: {
+          name: localStorage.getItem('playerName'),
+          role: 'keeper',
+          position: 5,
+          ready: false,
+          score: 0
+        },
+        guest: {
+          name: null,
+          role: 'shooter',
+          position: 5,
+          ready: false,
+          score: 0
+        },
+        round: 1,
+        start: false,
+        available: true
+      })
+      .then((docRef) => {
+          localStorage.setItem('room', docRef.id)
+          context.commit('CHANGE_ROOM', docRef.id)
+          router.push('/room/'+docRef.id)
+      })
+      .catch((error) => {
+          console.error("Error adding document: ", error)
+      })
+    },
+    joinRoom (context, payload) {
+      db.collection("rooms").doc(payload).update({
+          "guest.name": localStorage.getItem('playerName'),
+          available: false
+      })
+      .then(() => {
+          localStorage.setItem('room', payload)
+          router.push('/room/'+payload)
+      })
+      .catch((error) => {
+          console.error("Error updating document: ", error)
+      })
+    }
   },
   modules: {
   }
